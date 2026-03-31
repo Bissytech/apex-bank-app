e018788e25680ac4cd645574894f2924b11e7011
Bissytech <bisoye.ogunnaike@gmail.com>
2026-03-31 08:48:48 +0100

    added dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import App from "./App.tsx";
+import { HelmetProvider } from "react-helmet-async";
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.scss";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <HelmetProvider>
+        <Provider store={undefined}>
+          <PersistGate>
+            <MantineProvider>
+              <App />
+            </MantineProvider>
+          </PersistGate>
+        </Provider>
+      </HelmetProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
